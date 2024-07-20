155bf2caf9ce9b596180ae49b91dcae3da71da87
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-20 15:03:34 +0200

    inital commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const gameBoard = {
+  board: [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ]
+};
+
+const player1 = {}
+const player2 = {}
+
+const actions = {
+  placeMark: function() {
+    console.log("hello")
+  }
+}
+actions.placeMark()
