bdf50531438219df2bfbe852b9e9e2b705298af7
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-20 15:55:28 +0200

    setup generate board function

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const gameBoard = {
+  loadBoard: function() {
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody")
+    
+    for (let i = 0; i < this.board.length; i++) {
+      const row = document.createElement("tr");
+
+      for (let j = 0; j < this.board[i].length; j++) {
+        const cell = document.createElement("td");
+        const cellText = document.createTextNode(this.board[i][j])
+      cell.append(cellText)
+      row.append(cell)
+      }
+
+      tblBody.appendChild(row)
+    }
+
+    tbl.append(tblBody)
+    document.body.appendChild(tbl)
+
+    tbl.setAttribute("border", "2")
+  },
   board: [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
   ]
 };
 
@@ -15,3 +37,4 @@ const actions = {
   }
 }
 actions.placeMark()
+console.log(gameBoard.board)
